ac6ca7e2a85b4207fd74ead4635cf617b2f84177
Gijoop <luidku@gmail.com>
2025-11-05 20:18:18 +0100

    Start script fix

--- start.sh
+++ start.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+sudo chmod 666 /dev/hidraw*
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PYTHON_SCRIPT="$SCRIPT_DIR/main.py"
+
+VENV_DIR="$SCRIPT_DIR/.venv"
+LOG_FILE="$SCRIPT_DIR/spacemouse.log"
+
+# Creating enviroment
+if [ ! -d "$VENV_DIR" ]; then
+  python3 -m venv "$VENV_DIR"
+fi
+
+# Activating enviroment
+source "$VENV_DIR/bin/activate"
 
 echo "Starting SpaceMouse script..."
 python3 "$PYTHON_SCRIPT"
